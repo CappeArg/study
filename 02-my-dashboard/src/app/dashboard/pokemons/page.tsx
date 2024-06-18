0d4e46cd5bf89d75a591a1511f727697fb1d9f39
Pablo Cappellacci <pablocappe@gmail.com>
2024-06-18 05:07:35 -0300

    fin de sección

--- 02-my-dashboard/src/app/dashboard/pokemons/page.tsx
+++ 02-my-dashboard/src/app/dashboard/pokemons/page.tsx
@@ -1,8 +1,27 @@
+import { PokemonsGrid, PokemonsResponse, SimplePokemon } from "@/app/pokemons";
+import Image from "next/image";
 
-export default function PokemonsPage() {
+const getPockemons = async (limit=20, offset=0 ): Promise<SimplePokemon[]>=> {
+  const data: PokemonsResponse = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`)
+  .then(res => res.json())
+
+  const pokemons = data.results.map(pokemon => ({
+      id: pokemon.url.split('/').slice(-2)[0],
+      name: pokemon.name,
+  }))
+
+  // throw new Error ('Esto es un error que no debería ocurrir')
+
+  return pokemons 
+}
+
+export default async function PokemonsPage() {
+
+  const pokemons = await getPockemons(160);
   return (
-    <div>
-      <h1>Pokemons Page</h1>
+    <div className="flex flex-col">
+      <span className="text-5xl my-2">Listado de Pokemons <small>estático</small></span>
+      <PokemonsGrid pokemons={pokemons}/>
     </div>
   );
 }
---
+++ 02-my-dashboard/src/app/pokemons/components/PokemonsGrid.tsx
@@ -0,0 +1,21 @@
+import { SimplePokemon } from "@/app/pokemons"
+import { PokemonCard } from "./PokemonCard"
+
+interface Props {
+
+    pokemons: SimplePokemon[] 
+}
+
+export const PokemonsGrid = ({pokemons}:Props) => {
+  return (
+    <div className="flex flex-wrap gap-10 items-center justify-center">
+    { 
+      pokemons.map( pokemon =>(
+        <PokemonCard key={ pokemon.id } pokemon={pokemon} />
+        
+
+    
+    // <span key={pokemon.id}>cualquier cosa</span>
+    ))}
+    </div>  )
+}
